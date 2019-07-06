6f8ce3d9e188bdf03666a706d872652690331f03
Alec Nikolas Reiter <alecreiter@gmail.com>
2019-07-06 14:41:46 -0400

    Use signals to hit more places

--- src/opentracing_flask/__init__.py
+++ src/opentracing_flask/__init__.py
@@ -1,4 +1,4 @@
-from flask import current_app, request
+from flask import current_app, request, signals
 from opentracing import Format, child_of, global_tracer, tags
 from werkzeug.local import LocalProxy, LocalStack
 
@@ -54,10 +54,7 @@ class OpenTracingFlask:
             app.extensions = {}
 
         app.extensions["tracing"] = self
-
-        app.before_request(self._before_request)
-        app.after_request(self._after_request)
-        app.teardown_request(self._teardown_request)
+        self._instrument_app(app)
 
     @property
     def _tracer(self):
@@ -71,10 +68,37 @@ class OpenTracingFlask:
         return scope.span
 
     def end_active_span(self):
-        scope = self._span_manager.pop()
-        scope.close()
+        if current_span:
+            scope = self._span_manager.pop()
+            scope.close()
 
-    def _before_request(self):
+    def end_all_spans(self):
+        while self._span_manager.current is not None:
+            self.end_active_span()
+
+    def _instrument_app(self, app):
+        signals.template_rendered.connect(self._template_rendered, app)
+        signals.before_render_template.connect(self._before_template_rendered, app)
+        signals.request_started.connect(self._request_started, app)
+        signals.request_finished.connect(self._request_finished, app)
+        signals.got_request_exception.connect(self._got_request_exception, app)
+        signals.request_tearing_down.connect(self._request_tearing_down, app)
+        signals.appcontext_tearing_down.connect(self._appcontext_tearing_down, app)
+        signals.appcontext_pushed.connect(self._appcontext_pushed, app)
+        signals.appcontext_popped.connect(self._appcontext_popped, app)
+        signals.message_flashed.connect(self._message_flashed, app)
+        app.teardown_appcontext(lambda *_, **__: self.end_all_spans())
+
+    def _template_rendered(self, sender, template, context, **extras):
+        self.end_active_span()
+
+    def _before_template_rendered(self, sender, template, context, **extras):
+        template_name = template.name or "<STRING>"
+        operation = f"Jinja Rendering: {template_name}"
+        span = self.start_active_span(operation)
+        span.set_tag("Rendering Template", template_name)
+
+    def _request_started(self, sender, **extras):
         endpoint = request.endpoint if request.endpoint else "[UNMATCHED]"
 
         if endpoint == "static" and not self._trace_static:
@@ -95,25 +119,44 @@ class OpenTracingFlask:
 
         span.set_tag("endpoint", endpoint)
 
-    def _after_request(self, response):
+    def _request_finished(self, sender, response, **extras):
         span = current_span
 
         # we might not have started a span for this particular request
         if span:
             span.set_tag(tags.HTTP_STATUS_CODE, response.status_code)
 
-        return response
-
-    def _teardown_request(self, exception):
+    def _got_request_exception(self, sender, exception, **extras):
         span = current_span
 
         # we might not have started a span for this particular request
         if span:
-            if exception:
-                span.set_tag(tags.ERROR, True)
-                span.log_kv({"Kind": type(exception), "Message": str(exception)})
+            span.set_tag(tags.ERROR, True)
+            exc_type = type(exception)
 
-            self.end_active_span()
+            module = getattr(exception, "__module__", "")
+
+            name = exc_type.__name__
+
+            if module:
+                name = f"{module}.{exc_type.__name__}"
+
+            span.log_kv({"Type": name, "Message": str(exception)})
+
+    def _request_tearing_down(self, sender, **extras):
+        pass
+
+    def _appcontext_tearing_down(self, sender, **extras):
+        pass
+
+    def _appcontext_pushed(self, sender, **extras):
+        pass
+
+    def _appcontext_popped(self, sender, **extras):
+        pass
+
+    def _message_flashed(self, sender, message, category, **extras):
+        pass
 
 
 class _ActiveSpanManager:
@@ -129,6 +172,13 @@ class _ActiveSpanManager:
             )
 
         return rv[1]
+
+    @property
+    def current(self):
+        try:
+            return _tracing_span_ctx_stack.top[1]
+        except TypeError:
+            return None
 
 
 def __get_tracing():
